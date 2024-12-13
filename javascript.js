0c11e022d13a8b882e1562bf45020842ba594471
Matthew Maugeri <53546314+MattyMaugeri@users.noreply.github.com>
2024-12-13 11:36:22 +1100

    Completed basic HTML & CSS setup

--- javascript.js
+++ javascript.js
@@ -1,8 +1,39 @@
-const btns = document.querySelectorAll("button");
-btns.forEach((button) => {
-    button.addEventListener("click", () => {
-        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-        button.style.backgroundColor = "#" + randomColor;
-        console.log(randomColor);
-    });
-});
+const grid = document.querySelector('.grid');
+const clearBtnsContainer = document.querySelector('.clearBtnsContainer');
+const numberGrid = document.querySelector('.numberGrid');
+const symbolGrid = document.querySelector('.symbolGrid');
+
+const symbolArray = ['/', 'X', '-', '+', '='];
+
+
+function createGrid() {
+    const clearEntryBtn = document.createElement('button');
+    clearEntryBtn.textContent = 'CE';
+    clearEntryBtn.classList.add('clearBtn');
+    clearBtnsContainer.appendChild(clearEntryBtn);
+    const clearBtn = document.createElement('button');
+    clearBtn.textContent = 'C';
+    clearBtn.classList.add('clearBtn');
+    clearBtnsContainer.appendChild(clearBtn);
+
+    grid.appendChild(numberGrid);
+
+
+    for (let i = 9; i >= 0; i--) {
+        let number = document.createElement('button');
+        number.textContent = i;
+        number.classList.add('number');
+        number.classList.add(`n${i}`);
+        numberGrid.appendChild(number);
+    }
+
+    for (let i = 0; i < symbolArray.length; i++) {
+        let symbol = document.createElement('button');
+        symbol.classList.add('symbol');
+        symbol.classList.add(`s${i}`);
+        symbol.textContent = symbolArray[i];
+        symbolGrid.appendChild(symbol);
+    }
+}
+
+createGrid();
